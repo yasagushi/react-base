ac3f8d780e893d0212f3e94ef111f572ce3bdff7
vagrant <vagrant@web>
2019-04-29 03:46:50 +0900

    first commit

---
+++ webpack.config.babel.js
@@ -0,0 +1,32 @@
+import path from 'path';
+
+export default {
+  mode: 'development',
+  entry: `./src/js/app.js`,
+  output: {
+    path: path.join(__dirname, 'dist/js'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/react']
+            },
+          },
+          // {
+          //   enforce: 'pre',
+          //   test: /\.js$/,
+          //   exclude: /node_modules/,
+          //   loader: 'eslint-loader',
+          // },
+        ],
+      },
+    ],
+  },
+};
